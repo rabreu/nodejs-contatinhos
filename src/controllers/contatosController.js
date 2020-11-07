275477e5a942e7d88158939f81d6c217836f81dc
Renata Abreu <renata89abreu@gmail.com>
2020-11-07 16:44:54 -0300

    adicionado getById e getByNome

--- src/controllers/contatosController.js
+++ src/controllers/contatosController.js
@@ -26,7 +26,32 @@ const addContato = (request, response) => {
     })
 }
 
+const getById = (request, response) => {
+    console.log(`${request.method} ${request.url}`)
+    const id = request.params.id
+    contatoCollection.findById(id, (error, contato) => {
+        if(error)
+            return response.status(404).send({"mensagem": "Contatinho não existe. :("})
+        return response.status(200).send(contato)
+    })
+}
+
+const getByNome = (request, response) => {
+    console.log(`${request.method} ${request.url}`)
+    const nome = request.params.nome
+    contatoCollection.find({ "nome": nome }, (error, contato) => {
+        console.log(contato)
+        if(error)
+            return response.status(500).send(error)
+        if (contato.length > 0) 
+            return response.status(200).send(contato)
+        return response.status(404).send({"mensagem": "Contatinho não existe. :("})
+    })
+}
+
 module.exports = { 
     getAll,
-    addContato
+    addContato,
+    getById,
+    getByNome
 }
--- src/routes/contatosRoute.js
+++ src/routes/contatosRoute.js
@@ -4,5 +4,7 @@ const controller = require("../controllers/contatosController")
 
 router.get("/", controller.getAll)
 router.post("/", controller.addContato)
+router.get("/:id", controller.getById)
+router.get("/por_nome/:nome", controller.getByNome)
 
 module.exports = router
